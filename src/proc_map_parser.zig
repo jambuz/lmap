4bdda3e58d954be486b8a70265d9ee5afbb146c8
Your Name <you@example.com>
2025-09-20 21:07:10 +0300

    overhaul for 0.15.x

--- src/proc_map_parser.zig
+++ src/proc_map_parser.zig
@@ -1,105 +1,102 @@
 const std = @import("std");
 
 /// Linux process map parser
-pub const ProcessMapParser = struct {
-    arena: std.heap.ArenaAllocator,
-    allocator: std.mem.Allocator,
+pub fn ProcessMapParser(comptime max_maps: usize, comptime max_maps_file_len: usize) type {
+    return struct {
+        maps: [max_maps]Map = undefined,
 
-    /// Populated on initialization
-    /// Contains modules' base address
-    maps: std.ArrayList(Map),
-
-    const Perms = struct {
-        read: bool = false,
-        write: bool = false,
-        execute: bool = false,
-        private: bool = false,
-    };
-
-    const Map = struct {
-        start: usize,
-        end: usize,
-        perms: Perms,
-        path: []const u8,
-    };
-
-    pub const MAX_MAPS_LINE_LEN = 256;
-
-    pub fn init(allocator: std.mem.Allocator, pid: ?std.posix.pid_t) !ProcessMapParser {
-        var arena = std.heap.ArenaAllocator.init(allocator);
-
-        var maps = std.ArrayList(Map).init(arena.allocator());
-
-        const maps_file_path = if (pid) |p|
-            try std.fmt.allocPrint(arena.allocator(), "/proc/{d}/maps", .{p})
-        else
-            "/proc/self/maps";
-
-        const maps_file = try std.fs.openFileAbsolute(maps_file_path, .{ .mode = .read_only });
-        defer maps_file.close();
-
-        var buf: [MAX_MAPS_LINE_LEN]u8 = undefined;
-        var reader = std.io.bufferedReader(maps_file.reader());
-        while (try reader.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
-            if (try parseLine(arena.allocator(), line)) |map| try maps.append(map);
-        }
-
-        return ProcessMapParser{
-            .arena = arena,
-            .allocator = arena.allocator(),
-            .maps = maps,
+        const Perms = struct {
+            read: bool = false,
+            write: bool = false,
+            execute: bool = false,
+            private: bool = false,
         };
-    }
 
-    fn parseLine(allocator: std.mem.Allocator, line: []const u8) !?Map {
-        var tokens = std.mem.tokenizeScalar(u8, line, ' ');
+        const Map = struct {
+            start: usize,
+            end: usize,
+            perms: Perms,
+            path: []const u8,
+        };
 
-        const mem_range_str = tokens.next() orelse return null;
-        var mem_range = std.mem.splitScalar(u8, mem_range_str, '-');
+        // TODO: read all file contents into a fixed buf, then use std.mem.split for the final structure.
+        pub fn init(comptime pid: ?std.posix.pid_t) !@This() {
+            const maps_file_path = blk: {
+                if (pid) |p| break :blk std.fmt.comptimePrint("/proc/{}/maps", .{p});
+                break :blk "/proc/self/maps";
+            };
 
-        const start = std.fmt.parseInt(usize, mem_range.next() orelse return null, 16) catch return error.FailedToParseStartAddress;
-        const end = std.fmt.parseInt(usize, mem_range.next() orelse return null, 16) catch return error.FailedToParseEndAddress;
+            const maps_file = try std.fs.openFileAbsolute(maps_file_path, .{ .mode = .read_only });
+            defer maps_file.close();
 
-        const perms_str = tokens.next() orelse return null;
-        var perms = Perms{};
-        for (perms_str) |ch| {
-            switch (ch) {
-                'r' => perms.read = true,
-                'w' => perms.write = true,
-                'x' => perms.execute = true,
-                'p' => perms.private = true,
-                else => {},
+            var reader_buf: [max_maps_file_len]u8 = undefined;
+            var reader = maps_file.reader(&reader_buf);
+            const read_len = try reader.read(&reader_buf);
+
+            var maps_split = std.mem.splitScalar(u8, reader_buf[0..read_len], '\n');
+            var maps: [max_maps]Map = undefined;
+            while (maps_split.next()) |m| {
+                if (maps_split.index) |i| {
+                    const parsed = try parseLine(m);
+                    maps[i] = parsed;
+                }
             }
+
+            return .{
+                .maps = maps,
+            };
         }
 
-        _ = tokens.next();
-        _ = tokens.next();
-        _ = tokens.next();
+        fn parseLine(line: []const u8) !Map {
+            var tokens = std.mem.tokenizeScalar(u8, line, ' ');
 
-        const trimmed_path = std.mem.trim(u8, tokens.rest(), " ");
-        const path = try allocator.dupe(u8, trimmed_path);
+            const mem_range_str = tokens.next() orelse return error.FailedToParseMemRange;
+            var mem_range = std.mem.splitScalar(u8, mem_range_str, '-');
 
-        return Map{
-            .start = start,
-            .end = end,
-            .perms = perms,
-            .path = path,
-        };
-    }
+            const start = std.fmt.parseInt(usize, mem_range.next() orelse unreachable, 16) catch return error.FailedToParseStartAddress;
+            const end = std.fmt.parseInt(usize, mem_range.next() orelse unreachable, 16) catch return error.FailedToParseEndAddress;
 
-    pub fn deinit(self: *ProcessMapParser) void {
-        self.arena.deinit();
-    }
-};
+            const perms_str = tokens.next() orelse return error.FailedToParsePermissions;
+            var perms = Perms{};
+            for (perms_str) |ch| {
+                switch (ch) {
+                    'r' => perms.read = true,
+                    'w' => perms.write = true,
+                    'x' => perms.execute = true,
+                    'p' => perms.private = true,
+                    else => {},
+                }
+            }
+
+            _ = tokens.next();
+            _ = tokens.next();
+            _ = tokens.next();
+
+            const trimmed_path = std.mem.trim(u8, tokens.rest(), " ");
+            const path = trimmed_path;
+
+            return Map{
+                .start = start,
+                .end = end,
+                .perms = perms,
+                .path = path,
+            };
+        }
+
+        pub fn deinit(self: *@This()) void {
+            self.* = undefined;
+        }
+    };
+}
 
 test "Log all Maps of own process" {
-    const allocator = std.testing.allocator;
-
-    var p = try ProcessMapParser.init(allocator, null);
+    var p = try ProcessMapParser(128, 1 * 1024 * 1024).init(1706);
     defer p.deinit();
 
-    const maps = p.maps.items;
-    for (maps) |map| {
-        std.debug.print("{s}\n", .{map.path});
-    }
+    std.debug.print("{s}\n", .{p.maps});
+
+    // const maps = p.maps.items;
+    // for (maps) |map| {
+    //     std.debug.print("{s}\n", .{map.path});
+    // }
 }
