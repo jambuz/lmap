6e53bbe461e1c31200c016992fb65deaf76c4477
Your Name <you@example.com>
2025-10-26 01:42:57 +0300

    minimize build file

--- build.zig
+++ build.zig
@@ -1,13 +1,5 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-
-    const optimize = b.standardOptimizeOption(.{});
-
-    _ = b.addModule("lmap", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    _ = b.addModule("lmap", .{ .root_source_file = b.path("src/root.zig") });
 }
